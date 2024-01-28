5d564a1a452480db9fa0edfc6cd97079f610268e
Aryan Sharma <90350395+realaryansharma@users.noreply.github.com>
2024-01-28 16:07:08 +0530

    Some design changes

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -7,3 +7,7 @@ function closeUploadPopup() {
     document.getElementById("showPopup").style.visibility = "hidden";
     document.getElementById("showPopup").style.opacity = "0";
 }
+
+document.getElementById('logoutButton').onclick = function() {
+    window.location.href = '/logout';
+};
